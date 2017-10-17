2703c4f8d8d8ba4917f822e28c16831fc6c7ba1f
LawrenceXavier <lawrencealgorithm@gmail.com>
2017-10-18 00:29:19 +0700

    Add Graph.h, edit DFS.h and main.h

---
+++ 1_1/Graph.h
@@ -0,0 +1,34 @@
+#ifndef GRAPH_INCLUDED
+#define GRAPH_INCLUDED
+
+#include <cstdlib>
+
+struct Graph {
+	int n;
+	int s, t;
+	int** adj;
+	int* h; // heuristic function
+	Graph() {
+		this->n = 0;
+		this->s = this->t = -1;
+		this->adj = NULL;
+		this->h = NULL;
+	}
+
+	void initArray() {
+		if (this->n <= 0) return;
+		this->adj = (int**)calloc(this->n, sizeof(int*));
+		for (int i = 0; i < this->n; ++i) 
+			this->adj[i] = (int*)calloc(this->n, sizeof(int));
+		this->h = (int*)calloc(this->n, sizeof(int));
+	}
+
+	~Graph() {
+		free(this->h);
+		for (int i = 0; i < this->n; ++i)
+			free(this->adj[i]);
+		free(this->adj);
+	}
+};
+
+#endif
